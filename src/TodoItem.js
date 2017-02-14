9582d4273969219239ef9b8a4bfdfedd4e37ed3c
Max <contact@mstoiber.com>
2017-02-14 16:15:44 +0100

    Finish setup

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -1,14 +1,4 @@
-import React, { Component } from 'react';
-
 /**
  * This component should render a todo item as an <input type="checkbox" /> with a label
  */
-class TodoItem extends Component {
-  render() {
-    return (
-      <div>TodoItem</div>
-    );
-  }
-}
-
-export default TodoItem;
+import React, { Component } from 'react';
--- src/TodoList.js
+++ src/TodoList.js
@@ -1,16 +1,4 @@
-import React, { Component } from 'react';
-import TodoItem from './TodoItem';
-
 /**
  * This component should render the TodoItems inside a <li>
  */
-class TodoList extends Component {
-  render() {
-    console.log(this.props.items)
-    return (
-      <ul>TodoList</ul>
-    );
-  }
-}
-
-export default TodoList;
+import React, { Component } from 'react';
