095476344488ba07344f733548615f44e12fe870
Max <contact@mstoiber.com>
2017-02-15 14:51:04 +0100

    First setup

--- src/TodoList.js
+++ src/TodoList.js
@@ -2,3 +2,18 @@
  * This component should render the TodoItems inside a <li>
  */
 import React, { Component } from 'react';
+import TodoItem from './TodoItem';
+
+class TodoList extends Component {
+	render() {
+		return (
+			<ul>
+				{this.props.items.map(item => (
+					<li key={item.text}><TodoItem text={item.text} checked={item.checked} /></li>
+				))}
+			</ul>
+		)
+	}
+}
+
+export default TodoList;
