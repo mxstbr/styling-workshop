fe588205e09128a113cc10a2db80b7bbc332db1c
Max <contact@mstoiber.com>
2017-02-21 17:19:47 +0100

    Starting point

---
+++ src/Plot.js
@@ -0,0 +1,11 @@
+/**
+ * This component should render a plot with Plotly, taking these three props:
+ *
+ * - xData: the data to be rendered on the x-axis as an array
+ * - yData: The data to be rendered on the y-axis as an array
+ * - type:  The type of plot we want Plotly to render
+ *
+ * To see an example of how you should use Plotly look at the index.html file in the public/ folder.
+ * (and feel free to delete the code in there)
+ */
+import React, { Component } from 'react';
--- src/TodoInput.js
+++
@@ -1,4 +0,0 @@
-/**
- * This component should render a controlled input for text
- */
-import React, { Component } from 'react';
